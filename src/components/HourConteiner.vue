c6bf8387a9a2b0ef2b31cd78cec866e6fc4b11a8
99rp <bobkerman10@gmail.com>
2023-12-28 22:45:46 -0800

    Decrease hour icon sizes

--- src/components/HourConteiner.vue
+++ src/components/HourConteiner.vue
@@ -24,14 +24,19 @@ var dayORnight = ""
 <template>
     <div  :class="dayORnight" class="OneHourDiv">
         <HCTime :time="hour_obj.time"></HCTime>
-        <HCWeatherIcon :is_Day="hour_obj.is_day" :weather_code="hour_obj.weathercode"></HCWeatherIcon>
+        <HCWeatherIcon class="icons" :is_Day="hour_obj.is_day" :weather_code="hour_obj.weathercode"></HCWeatherIcon>
         <HCTemp :temp="hour_obj.temp"></HCTemp>
         <HCSmallInfo :temp_apparent="hour_obj.temp_app" :precipitation="hour_obj.precipitation" :wind_speed="hour_obj.wind_speed" :wind_direction="hour_obj.wind_dir"></HCSmallInfo>
-        <HCWindIcon :wind_speed="hour_obj.wind_speed" :wind_direction="hour_obj.wind_dir"></HCWindIcon>
+        <HCWindIcon class="icons" :wind_speed="hour_obj.wind_speed" :wind_direction="hour_obj.wind_dir"></HCWindIcon>
     </div>
 </template>
 
 <style scoped>
+
+.icons{
+    width: 50px;
+}
+
 .OneHourDiv{
     align-items: center;
     display: flex;
