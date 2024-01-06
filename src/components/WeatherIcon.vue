6963d5a217b3c7e91d6c81cf77ebd09f852a42b9
Vaaarna <127876080+Vaaarna@users.noreply.github.com>
2024-01-06 14:11:03 +0200

    dirty public test3

--- src/components/WeatherIcon.vue
+++ src/components/WeatherIcon.vue
@@ -52,8 +52,11 @@ if (!WeatherCodeMap.has(props.weather_code)) {
     }
 
 }
-
-// const imgURL = new URL(`./public/${weather_obj.name}`, import.meta.url).href;
+import arrow1 from "@/assets/icons/arrow_finalV3_rinalds_parpucis_9a.png"
+// const arrow1_url = new URL(`../../public/faveicon.png`, import.meta.url).href;
+const arrow1_url = new URL(`../../public/${weather_obj.name}`, import.meta.url).href;
+// const arrow1_url = new URL(`@/assets/icons/${weather_obj.name}`, import.meta.url).href;
+// const arrow1_url = new URL(`@/assets/icons/${weather_obj.name}`, import.meta.url).href;
 </script>
 
 <template>
@@ -62,7 +65,9 @@ if (!WeatherCodeMap.has(props.weather_code)) {
 
     <!-- <img :src="imgURL" alt="img" /> -->
      <!-- <img src="..public/icons/arrow_finalV3_rinalds_parpucis_9a.png"> -->
-    <img v-bind:src="`../public/${weather_obj.name}`" v-bind:alt="'weather icon:' + weather_obj.alt">
+    <!-- <img v-bind:src="`../public/${weather_obj.name}`" v-bind:alt="'weather icon:' + weather_obj.alt"> -->
+    <!-- <img v-bind:src="arrow1" v-bind:alt="'weather icon:' + weather_obj.alt"> -->
+    <img v-bind:src="arrow1_url" v-bind:alt="'weather icon:' + weather_obj.alt">
 </template>
 
 <style scoped>
