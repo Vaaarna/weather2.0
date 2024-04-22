1ae5122aeeab2ce793b3cc38fb1cbf126eef85f0
Vaaarna <127876080+Vaaarna@users.noreply.github.com>
2024-04-22 12:35:23 +0300

    minor renames

--- src/components/WeatherIcon.vue
+++ src/components/WeatherIcon.vue
@@ -5,32 +5,43 @@ const props = defineProps({
     isDay: Boolean,
 })
 const WeatherCodeMap = new Map([
-    [0, { name: "sun.png", alt: "clear" }],
-    [1, { name: "sunpartlycloudy.png", alt: "partly cloudy" }],
+    [0, { name: "sun.png", alt: "Clear sky" }],
+    [1, { name: "sunpartlycloudy.png", alt: "Mainly clear" }],
     [2, { name: "sunmostcloudy.png", alt: "partly cloudy" }],
     [3, { name: "cloud.png", alt: "overcast" }],
+
     [45, { name: "cloud.png", alt: "fog" }],
     [48, { name: "cloud.png", alt: "depositing rime fog " }],
+
     [51, { name: "drizzling.png", alt: "light drizzle" }],
     [53, { name: "drizzling.png", alt: "moderate drizzle" }],
     [55, { name: "raining.png", alt: "dense drizzle" }],
-    [56, { name: "raining.png", alt: "Light Freezing Rain" }],
-    [57, { name: "raining.png", alt: "dense Freezing Drizzle" }],
+
+    [56, { name: "raining.png", alt: "light freezing drizzle" }],
+    [57, { name: "raining.png", alt: "dense freezing drizzle" }],
+
     [61, { name: "raining.png", alt: "slight rain" }],
     [63, { name: "raining.png", alt: "moderate rain" }],
     [65, { name: "stormrainnn.png", alt: "heavy  rain" }],
-    [66, { name: "raining.png", alt: "Light Freezing Rain" }],
-    [67, { name: "stormrainnn.png", alt: "heavy intensity Freezing Rain" }],
-    [71, { name: "snow.png", alt: "Slight Snow fall" }],
-    [73, { name: "snow.png", alt: "moderate Snow fall" }],
-    [75, { name: "snow.png", alt: "heavy intensity Snow fall" }],
+
+    [66, { name: "raining.png", alt: "Light freezing rain" }],
+    [67, { name: "stormrainnn.png", alt: "heavy intensity freezing rain" }],
+
+    [71, { name: "snow.png", alt: "Slight snow fall" }],
+    [73, { name: "snow.png", alt: "moderate snow fall" }],
+    [75, { name: "snow.png", alt: "heavy intensity snow fall" }],
+
     [77, { name: "snow.png", alt: "Snow grains" }],
+
     [80, { name: "raining.png", alt: "slight rain showers" }],
     [81, { name: "raining.png", alt: "moderate rain showers" }],
     [82, { name: "stormrainnn.png", alt: "violent rain showers" }],
-    [85, { name: "snow.png", alt: "slight Snow showers" }],
-    [86, { name: "snow.png", alt: "heavy Snow showers" }],
+
+    [85, { name: "snow.png", alt: "slight snow showers" }],
+    [86, { name: "snow.png", alt: "heavy snow showers" }],
+
     [95, { name: "storm.png", alt: "Thunderstorm" }],
+
     [96, { name: "stormangy.png", alt: "Thunderstorm with slight hail" }],
     [99, { name: "stormangy.png", alt: "Thunderstorm with heavy hail" }],
 ])
