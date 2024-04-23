3b26b2c65cf16ca31173e087d278729afa85dfb7
Vaaarna <127876080+Vaaarna@users.noreply.github.com>
2024-04-23 14:45:55 +0300

    new icons :)

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 npm run dev
 
 TODO
-- add a domane
-- salabot lai strada icons
-- redo and add favicon (bike wheel? sun?)
-question mark weather icon if weathercodee is unknown
+- additional weather icons
+- question mark weather icon if weathercodee is unknown
 - finishing touches of design 
 - izvelne cik dienas https://vuejs.org/examples/#form-bindings
 https://vuejs.org/guide/essentials/watchers.html#watchers
-- opcijas vai ir offsets, cik dienas uz priekšu ( velak vietu )
+- opcijas vai ir offsets, cik dienas uz priekšu 
+- vietas izveele
+-
 - katrai sezonai, menesim, svetkos ari savs bg image
 
 DONE!!!:
 - fix the moon
 - show only future hours
-- order show 6 am as first hour after midnight show 1-5 am 
+- scrollable hours (not : order show 6 am as first hour after midnight show 1-5 am )
 - uhostot verceli 
-
+- add a domane
+- salabot lai strada icons
+- redo and add favicon (bike wheel? sun?)
--- src/components/WeatherIcon.vue
+++ src/components/WeatherIcon.vue
@@ -6,44 +6,44 @@ const props = defineProps({
 })
 const WeatherCodeMap = new Map([
     [0, { name: "sun.png", alt: "Clear sky" }],
-    [1, { name: "sunpartlycloudy.png", alt: "Mainly clear" }],
-    [2, { name: "sunmostcloudy.png", alt: "partly cloudy" }],
-    [3, { name: "cloud.png", alt: "overcast" }],
+    [1, { name: "sun Mainly clear.png", alt: "Mainly clear" }],
+    [2, { name: "sun partly cloudy.png", alt: "partly cloudy" }],
+    [3, { name: "overcast.png", alt: "overcast" }],
 
-    [45, { name: "cloud.png", alt: "fog" }],
-    [48, { name: "cloud.png", alt: "depositing rime fog " }],
+    [45, { name: "fog.png", alt: "fog" }],
+    [48, { name: "fog depositing rime copy.png", alt: "depositing rime fog " }],
 
     [51, { name: "drizzling.png", alt: "light drizzle" }],
-    [53, { name: "drizzling.png", alt: "moderate drizzle" }],
-    [55, { name: "raining.png", alt: "dense drizzle" }],
+    [53, { name: "drizzling moderate.png", alt: "moderate drizzle" }],
+    [55, { name: "drizzling dense.png", alt: "dense drizzle" }],
 
-    [56, { name: "raining.png", alt: "light freezing drizzle" }],
-    [57, { name: "raining.png", alt: "dense freezing drizzle" }],
+    [56, { name: "frozen drizzle.png", alt: "light freezing drizzle" }],
+    [57, { name: "dense frozen drizzle.png", alt: "dense freezing drizzle" }],
 
-    [61, { name: "raining.png", alt: "slight rain" }],
-    [63, { name: "raining.png", alt: "moderate rain" }],
-    [65, { name: "stormrainnn.png", alt: "heavy  rain" }],
+    [61, { name: "rain.png", alt: "slight rain" }],
+    [63, { name: "rain moderate .png", alt: "moderate rain" }],
+    [65, { name: "rain heavy.png", alt: "heavy  rain" }],
 
-    [66, { name: "raining.png", alt: "Light freezing rain" }],
-    [67, { name: "stormrainnn.png", alt: "heavy intensity freezing rain" }],
+    [66, { name: "rain freezing.png", alt: "Light freezing rain" }],
+    [67, { name: "rain freezing heavy.png", alt: "heavy intensity freezing rain" }],
 
-    [71, { name: "snow.png", alt: "Slight snow fall" }],
-    [73, { name: "snow.png", alt: "moderate snow fall" }],
-    [75, { name: "snow.png", alt: "heavy intensity snow fall" }],
+    [71, { name: "snow light.png", alt: "Slight snow fall" }],
+    [73, { name: "snow moderate.png", alt: "moderate snow fall" }],
+    [75, { name: "snow heavy.png", alt: "heavy intensity snow fall" }],
 
-    [77, { name: "snow.png", alt: "Snow grains" }],
+    [77, { name: "snow moderate.png", alt: "Snow grains" }],
 
-    [80, { name: "raining.png", alt: "slight rain showers" }],
-    [81, { name: "raining.png", alt: "moderate rain showers" }],
-    [82, { name: "stormrainnn.png", alt: "violent rain showers" }],
+    [80, { name: "rain showers.png", alt: "slight rain showers" }],
+    [81, { name: "rain showers moderate.png", alt: "moderate rain showers" }],
+    [82, { name: "rain showers heavy.png", alt: "violent rain showers" }],
 
-    [85, { name: "snow.png", alt: "slight snow showers" }],
-    [86, { name: "snow.png", alt: "heavy snow showers" }],
+    [85, { name: "snow showers light.png", alt: "slight snow showers" }],
+    [86, { name: "snow showers heavy.png", alt: "heavy snow showers" }],
 
-    [95, { name: "storm.png", alt: "Thunderstorm" }],
+    [95, { name: "thunderstorm.png", alt: "Thunderstorm" }],
 
-    [96, { name: "stormangy.png", alt: "Thunderstorm with slight hail" }],
-    [99, { name: "stormangy.png", alt: "Thunderstorm with heavy hail" }],
+    [96, { name: "thunderstorm slight hail.png", alt: "Thunderstorm with slight hail" }],
+    [99, { name: "thunderstorm heavyhail.png", alt: "Thunderstorm with heavy hail" }],
 ])
 var weather_obj
 // make a question mark weather png
@@ -54,11 +54,11 @@ if (!WeatherCodeMap.has(props.weather_code)) {
 
     if (props.isDay == false) {
         if (props.weather_code == 0) {
-            weather_obj = { name: "moonstars.png", alt: "clear" }
+            weather_obj = { name: "moon clear.png", alt: "clear" }
         } else if (props.weather_code == 1) {
-            weather_obj = { name: "moonstars.png", alt: "clear" }
+            weather_obj = { name: "moon mainly clear.png", alt: "mainly clear" }
         } else if (props.weather_code == 2) {
-            weather_obj = { name: "moonstars.png", alt: "clear" }
+            weather_obj = { name: "moon partly cloudy.png", alt: "partly cloudy" }
         }
     }
 
